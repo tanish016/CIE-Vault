1166ae11b0e84bf00fba7330ceed6cc013ed7393
tanish016 <tanishtp01@gmail.com>
2026-05-07 23:53:42 +0530

    add ppt

--- Client/src/app_components/register-form.tsx
+++ Client/src/app_components/register-form.tsx
@@ -125,7 +125,7 @@ export function RegisterForm() {
                   <User className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
                   <Input
                     id="name"
-                    placeholder="John Doe"
+                    placeholder="xyz"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     className="pl-10"
